68c4ad94ca2612ff1e0d934ae47ef9c25b991198
Roman Sorin <roman@romansorin.com>
2019-10-11 16:02:08 -0400

    More merge fixes

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
-const mix = require('laravel-mix');
+const mix = require('laravel-mix')
 
-require('laravel-mix-tailwind');
-require('laravel-mix-purgecss');
+require('laravel-mix-tailwind')
+require('laravel-mix-purgecss')
 
 /*
  |--------------------------------------------------------------------------
@@ -14,17 +14,11 @@ require('laravel-mix-purgecss');
  |
  */
 
-<<<<<<< HEAD
-mix.js('resources/js/app.js', 'public/js')
-   .postCss('resources/css/app.css', 'public/css')
-   .tailwind('./tailwind.config.js');
+mix
+  .js('resources/js/app.js', 'public/js')
+  .postCss('resources/css/app.css', 'public/css')
+  .tailwind('./tailwind.config.js')
 
 if (mix.inProduction()) {
-  mix
-   .version()
-   .purgeCss();
+  mix.version().purgeCss()
 }
-=======
-mix.react('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
->>>>>>> master
