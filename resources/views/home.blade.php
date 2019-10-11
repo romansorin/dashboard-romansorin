e599e395d4205e09f29bf1bf800bbe32ee67e4f9
Roman Sorin <roman@romansorin.com>
2019-10-11 15:59:47 -0400

    Fix merge conflicts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,48 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<<<<<<< HEAD
-    <div class="flex items-center">
-        <div class="md:w-1/2 md:mx-auto">
+<div class="flex items-center">
+    <div class="md:w-1/2 md:mx-auto">
 
-            @if (session('status'))
-                <div class="text-sm border border-t-8 rounded text-green-700 border-green-600 bg-green-100 px-3 py-4 mb-4" role="alert">
-                    {{ session('status') }}
-                </div>
-            @endif
+        @if (session('status'))
+        <div class="text-sm border border-t-8 rounded text-green-700 border-green-600 bg-green-100 px-3 py-4 mb-4"
+            role="alert">
+            {{ session('status') }}
+        </div>
+        @endif
 
-            <div class="flex flex-col break-words bg-white border border-2 rounded shadow-md">
+        <div class="flex flex-col break-words bg-white border border-2 rounded shadow-md">
 
-                <div class="font-semibold bg-gray-200 text-gray-700 py-3 px-6 mb-0">
-                    Dashboard
-                </div>
+            <div class="font-semibold bg-gray-200 text-gray-700 py-3 px-6 mb-0">
+                Dashboard
+            </div>
 
-                <div class="w-full p-6">
-                    <p class="text-gray-700">
-                        You are logged in!
-                    </p>
-=======
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
+            <div class="w-full p-6">
+                <p class="text-gray-700">
                     You are logged in!
->>>>>>> master
-                </div>
+                </p>
             </div>
         </div>
     </div>
-<<<<<<< HEAD
-=======
 </div>
->>>>>>> master
 @endsection
