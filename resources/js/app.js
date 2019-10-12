448789267878d3d9c99a43f23c3975ba6d29e621
Roman Sorin <roman@romansorin.com>
2019-10-12 14:40:04 -0400

    Base react-router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,13 @@
 import { hot } from 'react-hot-loader/root'
+import { Switch, Route } from 'react-router-dom'
 import React, { useEffect } from 'react'
 
 const App = () => {
-  return <div className='App' />
+  return (
+    <div className='App'>
+      <Switch />
+    </div>
+  )
 }
 
 export default hot(App)
--- resources/js/index.js
+++ resources/js/index.js
@@ -1,8 +1,11 @@
 import 'react-hot-loader'
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom'
 import axios from 'axios'
 import App from './App.js'
+import store from './store'
 
 const token = document.head.querySelector('meta[name="csrf-token"]')
 
@@ -11,7 +14,14 @@ axios.defaults.headers.common = {
   'X-Requested-With': 'XMLHttpRequest'
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root')
+)
 
 /**
  * Webpack Hot Module Replacement API
