47f0dde9cd4bf0a2049ba6522ec11e1494a62f48
Roman Sorin <roman@romansorin.com>
2019-10-19 16:19:19 -0400

    Use of env for existing

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -38,7 +38,7 @@ class UserTest extends TestCase
     {
         // Set exists to true if the Stripe customer exists and modify email accordingly
         $exists = true;
-        $existing_email = 'test@romansorin.com';
+        $existing_email = env('STRIPE_EXISTING_EMAIL');
 
         $user = factory(\App\User::class)->make();
         $user_arr = [
