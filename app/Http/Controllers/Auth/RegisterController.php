d02066f3f4d1c4e9c97fc15b6e8c6f9a204b0db4
Roman Sorin <roman@romansorin.com>
2019-10-19 13:53:37 -0400

    Added comment for confirmed

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,7 +53,10 @@ class RegisterController extends Controller
             'lastName' => ['required', 'string', 'max:255'],
             'username' => ['required', 'string', 'max:255', 'unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8'],
+            'password' => [
+                'required', 'string', 'min:8'
+                // 'confirmed'
+            ],
         ]);
     }
 
